9c6f7489e4f74e6fc9b75d591ccf316d71c84a97
J.Lord <followjaysin@gmail.com>
2022-07-07 22:46:06 -0500

    Added login and register page styling - now working on styling actual chat page

--- myapp/chat/routes.py
+++ myapp/chat/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from numpy import broadcast
 from myapp import socketio
 from flask_login import current_user
-from flask_socketio import emit
+from flask_socketio import emit, join_room, leave_room
 from myapp.models import History
 from myapp import db
 
@@ -25,3 +25,18 @@ def global_message(json):
     db.session.commit()
     msg = {"username": current_user.username, "msg": json}
     emit("global_message", msg, broadcast=True, )
+
+#  Add functionality so users know when new users join and leave chat 
+# @socketio.on('join', namespace='/chat')
+# def join(message):
+#     room = session.get('room')
+#     join_room(room)
+#     emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+
+# socketio.on('left', namespace='/chat')
+# def left(message):
+#     room = session.get('room')
+#     username = session.get('username')
+#     leave_room(room)
+#     session.clear()
+#     emit('status', {'msg': username + ' has left the room.'}, room=room)
